75d463f5fd96e377fafd64c2fad16ccae0ffb1e0
Cedric Ziel <cedric@cedric-ziel.com>
2024-01-03 19:48:08 +0100

    chore: set coverage target

--- test/MattermostTest.php
+++ test/MattermostTest.php
@@ -5,6 +5,9 @@ namespace CedricZiel\MattermostPhp\Test;
 use CedricZiel\MattermostPhp\Test\Container\MattermostContainer;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @covers \CedricZiel\MattermostPhp\Mattermost
+ */
 class MattermostTest extends TestCase
 {
     public function setUp(): void
