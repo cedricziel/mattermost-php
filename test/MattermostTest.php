9bad277a5a6527aa438218df8d107a5daa9bb6de
Cedric Ziel <cedric@cedric-ziel.com>
2024-01-03 19:40:58 +0100

    chore: enhance Mattermost.php

---
+++ test/MattermostTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CedricZiel\MattermostPhp\Test;
+
+use CedricZiel\MattermostPhp\Test\Container\MattermostContainer;
+use PHPUnit\Framework\TestCase;
+
+class MattermostTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+
+    public function testCanLogin()
+    {
+        $this->assertTrue(true);
+    }
+}
