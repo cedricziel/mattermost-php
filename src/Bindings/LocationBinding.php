6fb1fb12bb883a2d56217b13c74c23abf97a131d
Cedric Ziel <cedric@cedric-ziel.com>
2024-01-04 21:03:55 +0100

    fix: Location bindings

--- src/Bindings/LocationBinding.php
+++ src/Bindings/LocationBinding.php
@@ -10,23 +10,25 @@ use Symfony\Component\Serializer\Attribute\SerializedName;
 final class LocationBinding
 {
     public function __construct(
-        protected readonly string $icon,
-        protected readonly string $label,
-        protected readonly string $hint,
-        protected readonly string $description,
-        protected readonly Call $submit,
         /**
          * Location allows the App to identify where in the UX the Call request
          * comes from. It is optional. For /command bindings, Location is
          * defaulted to Label.
          */
         #[SerializedName('location')]
-        protected readonly ?Location $location = null,
-        protected readonly ?Form $form = null,
+        protected readonly string $location,
+        protected readonly string $icon,
+        protected readonly string $hint = '',
+        protected readonly string $description = '',
         /**
          * @var array<LocationBinding>
          */
+        #[SerializedName('bindings')]
         protected readonly ?array $bindings = null,
+        #[SerializedName('form')]
+        protected readonly ?Form $form = null,
+        protected readonly ?string $label = null,
+        protected readonly ?Call $submit = null,
     ) {
     }
 }
