54cbab8c284a96f75c2a05ba40186658d3c0f6db
Cedric Ziel <cedric@cedric-ziel.com>
2024-01-04 22:26:05 +0100

    fix: Namespace for Form (#13)

--- src/Response/CallResponse.php
+++ src/Response/CallResponse.php
@@ -2,7 +2,7 @@
 
 namespace CedricZiel\MattermostPhp\Response;
 
-use App\Mattermost\Form\Form;
+use CedricZiel\MattermostPhp\Form\Form;
 use Symfony\Component\Serializer\Attribute\SerializedName;
 
 abstract class CallResponse
