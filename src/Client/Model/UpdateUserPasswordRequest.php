0f2b70b6fb5539cf46ffc7fcd3e7183dc5d8b33c
Cedric Ziel <cedric@cedric-ziel.com>
2024-05-19 12:22:50 +0200

    chore: correctly order required parameters

--- src/Client/Model/UpdateUserPasswordRequest.php
+++ src/Client/Model/UpdateUserPasswordRequest.php
@@ -5,10 +5,10 @@ namespace CedricZiel\MattermostPhp\Client\Model;
 class UpdateUserPasswordRequest
 {
     public function __construct(
-        /** The current password for the user */
-        public ?string $current_password = null,
         /** The new password for the user */
         public string $new_password,
+        /** The current password for the user */
+        public ?string $current_password = null,
     ) {
     }
 }
